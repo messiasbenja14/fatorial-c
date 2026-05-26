200e22d6888e8927af0baa2da7c7aaa9514c10fe
messiasbenja14 <messias.benja14@gmail.com>
2026-05-26 18:49:45 -0300

    Update fatorial.c

--- fatorial.c
+++ fatorial.c
@@ -7,6 +7,8 @@ long fatorial = 1;
 
 printf("digite um numero\n");
 scanf("%d",&n);
+
+neymar = fatorial(n);
     
 printf("o fatorial de %d é %d \n", n, fatorial);
 printf("feito por benjamim messias");
